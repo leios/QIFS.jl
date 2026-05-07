76443afdf1b89b54efa0b9b872d4ee99310e4782
James Schloss <jrs.schloss@gmail.com>
2026-05-07 11:34:41 +0200

    adding example_3

--- src/classical.jl
+++ src/classical.jl
@@ -9,6 +9,10 @@ cantor_1(x) = x/3
 
 cantor_2(x) = x/3 + 2/3
 
+#=-----------------------------------------------------------------------------#
+EXAMPLE 1
+#-----------------------------------------------------------------------------=#
+
 function example_1()
     pt = rand()
     res = 1000
@@ -21,6 +25,10 @@ function example_1()
 
     return arr
 end
+
+#=-----------------------------------------------------------------------------#
+EXAMPLE 2
+#-----------------------------------------------------------------------------=#
 
 function select_fx_num(probs::Tuple)
     if !isapprox(sum(probs), 1)
@@ -50,6 +58,51 @@ function example_2()
         pt = fxs[choice](pt)
         bin = floor(Int, pt*res)+1
         arr[bin] += 1
+    end
+
+    return arr
+end
+
+#=-----------------------------------------------------------------------------#
+EXAMPLE 3
+#-----------------------------------------------------------------------------=#
+
+struct Point{FT <: Real}
+    x::FT
+    y::FT
+end
+
+function tartan_1(pt::Point)
+    out = [1/3 0.; 0. 1.]*[pt.x, pt.y]
+    return Point(out[1], out[2])
+end
+
+function tartan_2(pt::Point)
+    out = [1/3 0.; 0. 1.]*[pt.x, pt.y] .+ [2/3, 0]
+    return Point(out[1], out[2])
+end
+
+function tartan_3(pt::Point)
+    out = [1. 0.; 0. 1/3]*[pt.x, pt.y]
+    return Point(out[1], out[2])
+end
+
+function tartan_4(pt::Point)
+    out = [1. 0.; 0. 1/3]*[pt.x, pt.y] .+ [0, 2/3]
+    return Point(out[1], out[2])
+end
+
+function example_3()
+    pt = Point(rand(), rand())
+    res = 1000
+    arr = zeros(res, res)
+    choice = 0
+    fxs = (tartan_1, tartan_2, tartan_3, tartan_4)
+    for i = 1:10000000
+        pt = rand(fxs)(pt)
+        bin_x = floor(Int, pt.x*res)+1
+        bin_y = floor(Int, pt.y*res)+1
+        arr[bin_x, bin_y] += 1
     end
 
     return arr
