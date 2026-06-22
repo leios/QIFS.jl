e37ff1f7913ef3bf7bcf72d6a291e9b1630989b2
James Schloss <jrs.schloss@gmail.com>
2026-06-22 13:56:56 +0200

    adding simulated gaussian wo shots

--- python/husimi_hardware.py
+++ python/husimi_hardware.py
@@ -8,25 +8,39 @@ from qiskit_ibm_runtime import QiskitRuntimeService
 from qiskit.quantum_info import DensityMatrix, state_fidelity
 from qiskit_experiments.library import StateTomography
 
+def make_displacement_circuit(x, y, xmax, ymax, qmr, qr):
+    x_ratio = 2*numpy.pi*x/xmax
+    y_ratio = 2*numpy.pi*y/xmax
+    move_ref_circuit = bosonic_qiskit.CVCircuit(qmr, qr)
+    move_ref_circuit.cv_d(-numpy.pi+x_ratio+(-numpy.pi+y_ratio)*1j,qmr[0])
+    return move_ref_circuit
+
 def append_displacement_circuit(x, y, xmax, ymax, qmr, circuit):
     x_ratio = 2*numpy.pi*x/xmax
     y_ratio = 2*numpy.pi*y/xmax
     circuit.cv_d(-numpy.pi+x_ratio+(-numpy.pi+y_ratio)*1j,qmr[0])
 
-def simple_husimi(xmax, ymax, backend, dens_mat, num_shots=1024):
+# maybe partial trace?
+def simple_husimi(xmax, ymax, backend, dens_mat, nqpm):
     fids = numpy.zeros((xmax, ymax))
+
+    (qmr_ref, qr_ref, c_ref) = init_gaussian(nqpm)
+
+    state_ref, _, _ = bosonic_qiskit.util.simulate(c_ref)
+
     for i in range(0,xmax):
         for j in range(0,ymax):
-            (qmr1, qr1, init_ref_c) = init_gaussian(4)
-            append_displacement_circuit(i, j, xmax, ymax, qmr1, init_ref_c)
-            init_ref_ch = decompose_init_circuit(init_ref_c)
+            c_move_ref = make_displacement_circuit(i, j, xmax, ymax, qmr_ref, qr_ref)
+            state_ref, _, _ = bosonic_qiskit.util.simulate(c_move_ref)
 
-            tomo_ref = StateTomography(init_ref_c, measurement_indices=[0,1,2,3])
-            result_ref = tomo_ref.run(backend, shots = num_shots).block_for_results()
-            dens_mat_ref = result_ref.analysis_results("state", dataframe = True).iloc[0].value
+            dens_mat_ref = qiskit.quantum_info.DensityMatrix(state_ref)
 
-            fids[i][j] = state_fidelity(dens_mat, dens_mat_ref)
-            print(fids[i][j])
+            # trace out ancillary bit
+            dens_mat_ref_partial = qiskit.quantum_info.partial_trace(dens_mat_ref, [nqpm])
+            #dens_mat1 = qiskit.quantum_info.partial_trace(full_dens_mat1, [1])
+
+            fids[i][j] = state_fidelity(dens_mat, dens_mat_ref_partial)
+            #print(fids[i][j])
     return fids
 
 def find_backend():
@@ -49,6 +63,18 @@ def init_gaussian(nqpm):
 
     return (qmr, qr, init_c)
 
+# N Number of qubits
+# M size of circle M < N
+def init_circle(N, M):
+
+    dephasing_ops = [
+        np.array([[1, 0], [0, 0]]),
+        np.array([[0, 0], [0, 1]])
+    ]
+    dephasing_channel = Kraus(dephasing_ops)
+
+    
+
 # removing unnecessary resets from circuit after decomposition
 def decompose_init_circuit(init_c):
     # ch = "circuit hardware"
@@ -59,20 +85,20 @@ def decompose_init_circuit(init_c):
     ]
     return init_ch
 
-def qifs(num_shots=1024):
-    (qmr0, qr0, init_state_c) = init_gaussian(4)
+def qifs(num_shots=1024, nqpm = 4):
+    (qmr0, qr0, init_state_c) = init_gaussian(nqpm)
 
     init_state_ch = decompose_init_circuit(init_state_c)
 
     backend = find_backend()
 
-    tomo_state = StateTomography(init_state_c, measurement_indices=[0,1,2,3])
+    tomo_state = StateTomography(init_state_c, measurement_indices=[i for i in range(0, nqpm)])
 
     result_state = tomo_state.run(backend, shots = num_shots).block_for_results()
 
     dens_mat_state = result_state.analysis_results("state", dataframe = True).iloc[0].value
 
-    fids = simple_husimi(10, 10, backend, dens_mat_state, num_shots = num_shots)
+    fids = simple_husimi(50,50, backend, dens_mat_state, nqpm)
 
     plt.imshow(fids, cmap='hot', interpolation='nearest')
     plt.show()
@@ -83,4 +109,4 @@ def qifs(num_shots=1024):
     #dens_mat_state_ideal = tomo_state_ideal.run(ideal_backend, shots = num_shots).block_for_results().analysis_results("state", dataframe = True).iloc[0].value
     #fids = simple_husimi(5, 5, backend, dens_mat_state)
 
-#qifs()
+qifs()
